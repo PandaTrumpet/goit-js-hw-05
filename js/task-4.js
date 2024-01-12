41eb154f1f7dff2b8413149c7ca03d8346c7770a
PandaTrumpet <dsavontrumpet@gmail.com>
2024-01-12 10:38:44 +0200

    update

--- js/task-4.js
+++ js/task-4.js
@@ -1,32 +1,52 @@
 'use strict';
-function getShippingCost(country) {
-  let price;
-
-  switch (country) {
-    case 'China':
-      price = 100;
-      break;
-    case 'Chile':
-      price = 250;
-      break;
-    case 'Australia':
-      price = 170;
-
-      break;
-    case 'Jamaica':
-      price = 120;
-
-      break;
-
-    default:
-      return 'Sorry, there is no delivery to your country';
+const getTotalBalanceByGender = (users, gender) => {
+      let total = 0
+   users
+  .filter(user => user.gender === gender)
+  .forEach((number) => {
+      total += number.balance
+  })
+  return total
   }
-  return `Shipping to ${country} will cost ${price} credits`;
-}
-
-console.log(getShippingCost('Australia'));
-console.log(getShippingCost('Germany'));
-console.log(getShippingCost('China'));
-console.log(getShippingCost('Chile'));
-console.log(getShippingCost('Jamaica'));
-console.log(getShippingCost('Sweden'));
+  
+  const allUsers = [
+      {
+      name: "Moore Hensley",
+      gender: "male",
+      balance: 2811
+    },
+    {
+      name: "Sharlene Bush",
+      gender: "female",
+      balance: 3821
+    },
+    {
+      name: "Ross Vazquez",
+      gender: "male",
+      balance: 3793
+    },
+    {
+      name: "Elma Head",
+      gender: "female",
+      balance: 2278
+    },
+    {
+      name: "Carey Barr",
+      gender: "male",
+      balance: 3951
+    },
+    {
+      name: "Blackburn Dotson",
+      gender: "male",
+      balance: 1498
+    },
+    {
+      name: "Sheree Anthony",
+      gender: "female",
+      balance: 2764
+    }
+  ];
+  
+  console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+  
+  console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
